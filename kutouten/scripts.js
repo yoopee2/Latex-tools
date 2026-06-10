180b84d727377df7900296bb3ef506aea6e298c8
yoopee2 <anhahuwai2@gmail.com>
2026-06-10 19:45:25 +0900

    scripts.js を更新

--- kutouten/scripts.js
+++ kutouten/scripts.js
@@ -5,28 +5,47 @@ trans.addEventListener('click',()=>{
 });
 
 function exe(){
+
 	var out = document.getElementById('text').value;
-	
-	const tenCheck = document.getElementById('kutouten').checked;
-	if(tenCheck){
-		out = out.replace(/、/g,'，');
-		out = out.replace(/。/g,'．');
+	var fout = document.getElementById('file');
+	if(fout.files.length > 0){
+		const file = fout.files[0];
+		var reader = new FileReader();
+		let out = "";
+
+		reader.onload = function(e){
+			let fileout = e.target.result;
+			fix(fileout);
+		}
+		reader.readAsText(file);
+	}
+	else if(out.trim() != ""){
+		fix(out);
 	}
 	
-	const spaceCheck = document.getElementById('space').checked;
-	if(spaceCheck){
-		out = out.replace(/　/g,' ');
+	
+	function fix(out){
+		const tenCheck = document.getElementById('kutouten').checked;
+		if(tenCheck){
+			out = out.replace(/、/g,'，');
+			out = out.replace(/。/g,'．');
+		}
+	
+		const spaceCheck = document.getElementById('space').checked;
+		if(spaceCheck){
+			out = out.replace(/　/g,' ');
 	}
 	
-	const zenkaku = document.getElementById('zenkaku').checked;
-	if(zenkaku){
-		out = out.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(c){
-			var charCode = c.codePointAt(0);
-			charCode -= 65248;
-			const hankaku = String.fromCodePoint(charCode);
-			return hankaku;
+		const zenkaku = document.getElementById('zenkaku').checked;
+		if(zenkaku){
+			out = out.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(c){
+				var charCode = c.codePointAt(0);
+				charCode -= 65248;
+				const hankaku = String.fromCodePoint(charCode);
+				return hankaku;
 		});
 	}
 	
-		document.getElementById('output').value = out;
+			document.getElementById('output').value = out;
+	}
 }
